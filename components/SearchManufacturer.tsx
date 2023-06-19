9d68167f4d9b65cc08a5e6990fbefebd5fe9ae18
ilyaas <ilyaascumar10@gmail.com>
2023-06-19 14:21:27 +0300

    rapidapi starting

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -19,11 +19,11 @@ const SearchManufacturer = ({
             .replace(/\s+/g, "")
             .includes(query.toLocaleLowerCase().replace(/\s+/g, ""))
         );
-        console.log(filteredManufacturers);
-        
+  console.log(filteredManufacturers);
+
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image
@@ -60,7 +60,24 @@ const SearchManufacturer = ({
                     }
                     value={item}
                   >
-                    {item}
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? "font-medium" : "font-normal"
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? "text-white" : "text-teal-600"
+                            }`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
                   </Combobox.Option>
                 ))
               }
---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd942561f50msh3c03ed234072d22p16298ajsn830cb23ad5e7',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
